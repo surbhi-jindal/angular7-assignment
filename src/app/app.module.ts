74f86fb5ff658be8644cd636d1e84582db3e0f30
surbhi-jindal <49064323+surbhi-jindal@users.noreply.github.com>
2019-04-08 17:16:33 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PublicPagesModule} from './public-pages/public-pages.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { Routes} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicPagesModule,
+    ReactiveFormsModule ,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
