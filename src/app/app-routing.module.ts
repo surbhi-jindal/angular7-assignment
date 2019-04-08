017aa631178e921f7a43daf496b8b5dded53bdcf
surbhi-jindal <49064323+surbhi-jindal@users.noreply.github.com>
2019-04-08 17:12:48 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'; 
+import { Routes, RouterModule } from '@angular/router'; 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+{ 
+path: 'public', 
+loadChildren: './public-pages/public-pages.module#PublicPagesModule' 
+}, 
+{ 
+path: 'user', 
+loadChildren: './private-pages/private-pages.module#PrivatePagesModule' 
+}, 
+{ 
+path: '', 
+redirectTo: '/public/register', 
+pathMatch: 'full' 
+} 
+]; 
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+@NgModule({ 
+imports: [RouterModule.forRoot(routes)], 
+exports: [RouterModule] 
+}) 
 export class AppRoutingModule { }
+
